155d6b36d9e48b4b9a0ece47598337aebe5d4a4f
PC <PC@DESKTOP-CQBP31U>
2023-01-12 10:32:49 +0900

    testtest

---
+++ aa/src/CategoryVo.java
@@ -0,0 +1,21 @@
+package com.category.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.validation.constraints.NotNull;
+
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CategoryVo {
+
+    @Schema(nullable = false, example = "steve")
+    @NotNull
+    private String categoryName;
+
+    @Schema(nullable = true, example = "1")
+    private Long parentId;
+}
